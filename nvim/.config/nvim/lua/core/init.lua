4d169fe3d184368b2fdb966cb34861e9cdfe0306
haakonfp <haakonsnexus@gmail.com>
2024-03-30 17:01:06 +0100

    feat: set wrap false for now

--- nvim/.config/nvim/lua/core/init.lua
+++ nvim/.config/nvim/lua/core/init.lua
@@ -11,6 +11,7 @@ g.transparency = config.ui.transparency
 -------------------------------------- options ------------------------------------------
 opt.laststatus = 3 -- global statusline
 opt.showmode = false
+opt.wrap = false
 
 opt.clipboard = "unnamedplus"
 opt.cursorline = true
