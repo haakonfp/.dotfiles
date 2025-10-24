60afa44c657504271914cb7df185c63048965b04
haakonfp <haakonsnexus@gmail.com>
2025-10-24 08:24:32 +0200

    feat(nvim): enable copilot auto trigger

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -12,6 +12,8 @@ return {
     config = function()
       require("copilot").setup {
         suggestion = {
+          enabled = true,
+          auto_trigger = true,
           keymap = {
             accept = "<M-l>", -- Alt + l
           },
